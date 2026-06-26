dc475651f4e79d648e2e4f707ab1f7bc014f5e40
Mohamud Qanyare <mohamud.qanyare@gmail.com>
2026-06-26 21:45:56 +0100

    This is ch1_intro Javascript

---
+++ ch1_intro/script.js
@@ -0,0 +1,5 @@
+console.log("Hello World")
+console.log("I am a Football fan");
+console.log("The World Cup 2026 has Started");
+console.log("Hope England Wins, IS COMING HOME");
+console.log("Thanks To Fasttrack Technology Academy This Amazing Knowledge");
