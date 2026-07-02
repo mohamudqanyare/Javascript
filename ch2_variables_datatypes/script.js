1fe24ef934f6befb3114f39a22bd2783c461404f
Mohamud Qanyare <mohamud.qanyare@gmail.com>
2026-07-02 23:40:42 +0100

    I've completed ch2_variable_datatypes

---
+++ ch2_variables_datatypes/script.js
@@ -0,0 +1,43 @@
+var town = "Birmingham" // string
+console. log(town);
+
+var firstName = "Mohamud Yusuf" // string
+console. log(firstName)
+
+var lastName = "Qanyare" // string
+console. log(lastName) // string
+
+var age = 39 // number
+console. log(age)
+
+var isStudent = true; // boolean
+console. log(isStudent)
+
+console. log(age + 5)
+
+console. log(firstName * lastName)
+
+console.log(typeof town)
+
+console. log(typeof age)
+
+console. log(typeof isStudent)
+
+// Remember these are Javascript Reserved words
+// abstract 	arguments 	async * 	await *
+// boolean 	break 	byte 	case
+// catch 	char 	class * 	const *
+// continue 	debugger 	default 	delete
+// do 	double 	else 	enum *
+// eval 	export * 	extends * 	false
+// final 	finally 	float 	for
+// function 	goto 	if 	implements
+// function 	import * 	in 	instanceof
+// int 	interface 	let * 	long
+// native 	new 	null 	package
+// private 	protected 	public * 	return
+// short 	static * 	super * 	switch
+// synchronized 	this 	throw 	throws
+// transient 	true 	try 	typeof
+// using * 	var 	void 	volatile
+// while 	with 	yield *
