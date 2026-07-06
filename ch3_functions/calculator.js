a082ad1b73085fec25a671b66d93f8f005344646
Mohamud Qanyare <mohamud.qanyare@gmail.com>
2026-07-06 07:28:38 +0100

    I've learnt and completed ch3_functions

---
+++ ch3_functions/calculator.js
@@ -0,0 +1,116 @@
+function addition() {
+  var num1 = 1000;
+  var num2 = 10;
+  console.log("the result is", num1 + num2);
+}
+addition();
+
+function subtruction() {
+  var num1 = 1000;
+  var num2 = 10;
+  console.log("the result is", num1 - num2);
+}
+subtruction();
+
+function multiply() {
+  var num1 = 1000;
+  var num2 = 10;
+  console.log("the result is", num1 * num2);
+}
+multiply();
+
+function divition() {
+  var num1 = 1000;
+  var num2 = 10;
+  console.log("the result is", num1 / num2);
+}
+divition();
+
+
+function multiply(num1,num2){
+    return num1 * num2
+}
+var result1 = multiply(8,8)
+console.log(result1);
+
+var result2 = multiply(5,5)
+console.log(result2);
+
+var result3 = multiply(6,3)
+console.log(result3);
+
+var result4 = multiply(2,4)
+console.log(result4);
+
+var result5 = multiply(4,4)
+console.log(result5);
+
+var result6 = multiply(3,3)
+console.log(result6);
+
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+var multiply = function (num1, num2) {
+  return num1 * num2;
+};
+
+var result1 = multiply(8, 8);
+console.log(result1);
+
+var result2 = multiply(5, 5);
+console.log(result2);
+
+var result3 = multiply(6, 3);
+console.log(result3);
+
+var result4 = multiply(2, 4);
+console.log(result4);
+
+var result5 = multiply(4, 4);
+console.log(result5);
+
+var result6 = multiply(3, 3);
+console.log(result6);
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+var result1 = add(3, 3);
+console.log(result1);
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+var result1 = subtract(8, 5);
+console.log(result1);
+
+function addition() {
+  var num1 = 1000;
+  var num2 = 10;
+  console.log("the result is", num1 + num2);
+}
+addition();
+
+function subtruction() {
+  var num1 = 1000;
+  var num2 = 10;
+  console.log("the result is", num1 - num2);
+}
+subtruction();
+
+function multiply() {
+  var num1 = 1000;
+  var num2 = 10;
+  console.log("the result is", num1 * num2);
+}
+multiply();
+
+function divition() {
+  var num1 = 1000;
+  var num2 = 10;
+  console.log("the result is", num1 / num2);
+}
+divition();
---
+++ ch3_functions/script.js
@@ -0,0 +1,37 @@
+function days(){
+  console.log("Monday");
+  console.log("Tuesday");
+  console.log("Wednesday");
+  console.log("Thursday");
+  console.log("Friday");
+  console.log("Saturday");
+  console.log("Sunday");
+}
+
+function months(){
+  console.log("January");
+  console.log("February");
+  console.log("March");
+  console.log("April");
+  console.log("May");
+  console.log("June");
+  console.log("July");
+  console.log("August");
+  console.log("September");
+  console.log("October");
+  console.log("November");
+  console.log("December");
+}
+
+function bestTeams(){
+  console.log("Liverpool");
+  console.log("Arsenal");
+  console.log("Man City");
+  console.log("Chelsea");
+  console.log("Newcastle");
+}
+
+
+days()
+months()
+bestTeams()
