b9e9c5621138751dc3355233cd4544724378e866
Mohamud Qanyare <mohamud.qanyare@gmail.com>
2026-07-17 12:05:56 +0100

    I've learnt ch5_let_var_const_scope

---
+++ ch5_let_var_const_scope/script.js
@@ -0,0 +1,17 @@
+// var firstName = "John";
+// firstName = "Mohamud";
+// console.log(firstName);
+
+// let firstName = "John";
+// firstName = "Mohamud";
+// console.log(firstName);
+
+// const firstName = "John";
+// console.log(firstName);
+
+let age = 5;
+console.log(age);
+{
+    // let lastName = "Doe" //local scope
+    // console.log(lastName)
+}
---
+++ ch6_conditional_statements/if_else.js
@@ -0,0 +1,13 @@
+let france = false;
+console.log("London");
+console.log("New York");
+console.log("Tokyo");
+console.log("Sydney");
+console.log("Berlin");
+if(france === true) {
+    console.log("Paris");
+}
+console.log("Mosco");
+console.log("Rio de Janeiro");
+console.log("Cape Town");
+console.log("Toronto");
