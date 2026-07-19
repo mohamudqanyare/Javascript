b488445d95c9fe22bd6d3f99fe56a73852c2ac6a
Mohamud Qanyare <mohamud.qanyare@gmail.com>
2026-07-19 03:34:51 +0100

    i've learnt ch10_Dom and ch11_ document javascript object

---
+++ ch10_dom/script.js
@@ -0,0 +1,21 @@
+// for (let i = 0; i < 100; i++) {
+//   let h1 = document.createElement("h1");
+//   h1.textContent = i + " I was created by javascript";
+//   h1.style.color = "red";
+//   document.body.appendChild(h1);
+// }
+
+// let div = document.createElement("div")
+// div.textContent = "I am a div"
+// document.body.appendChild(div)
+// document.body.removeChild(div)
+
+// let secondH1 = document.getElementById("second")
+// secondH1.style.fontSize = "20px"
+// secondH1.style.textAlign = "center"
+// secondH1.style.backgroundColor = "gold"
+
+let infos = document.getElementsByName("info")
+infos[0].style.fontSize = "100px"
+
+infos[1].style.color = "red"
---
+++ ch11_javascript_objects/script.js
@@ -0,0 +1,17 @@
+let student = {
+    firstName: "Mohamud",
+    lastName: "Qanyare",
+    age: 40,
+    color: "blue",
+}
+
+// console.log (student.firstName)
+// console.log (student.lastName)
+// console.log (student.age)
+// console.log (student.color)
+student.nationality = "British"
+student.isMarried = true
+// console.log(student.nationality)
+
+console.log(student["nationality"])
+console.log(student["isMarried"])
