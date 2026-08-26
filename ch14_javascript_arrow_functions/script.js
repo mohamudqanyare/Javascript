c13f18921ef92020c60b431e37c8146ae3c30f7e
Mohamud Qanyare <mohamud.qanyare@gmail.com>
2026-08-26 13:59:19 +0300

    I've completed chapter 14 Javascript_Arrow_Functions

---
+++ ch14_javascript_arrow_functions/script.js
@@ -0,0 +1,18 @@
+// 
+// traditional function
+// function multiply(num1,num2){
+//     return num1 * num2
+// }
+
+
+// variable function
+// let multiply = function (num1,num2){
+//     return num1 * num2
+// }
+
+// arrow function
+let multiply = (num1,num2)=> num1 * num2
+
+
+let result = multiply(2,5)
+console.log(result)
