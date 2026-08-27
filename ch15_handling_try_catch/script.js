2f87018ca74123d88a537857d5d91aaa026520e7
Mohamud Qanyare <mohamud.qanyare@gmail.com>
2026-08-27 14:24:02 +0300

    I completed chapter 15 Handling_Try_Catch

---
+++ ch15_handling_try_catch/script.js
@@ -0,0 +1,9 @@
+try{
+// let firstName 
+let firstName = "Mohamud"
+console.log(firstName.toUpperCase())
+} catch(error){
+    console.log(error.message)
+} finally{
+    console.log("i will run everytime regardles of error or not")
+}
